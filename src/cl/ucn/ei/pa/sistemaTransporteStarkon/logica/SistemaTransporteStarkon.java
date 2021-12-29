01900539cb0a717894e6cbe82e512cb40aa70fac
NullCoder <90267124+null0010@users.noreply.github.com>
2021-12-29 09:01:15 +0900

    Update SistemaTransporteStarkon.java

--- src/cl/ucn/ei/pa/sistemaTransporteStarkon/logica/SistemaTransporteStarkon.java
+++ src/cl/ucn/ei/pa/sistemaTransporteStarkon/logica/SistemaTransporteStarkon.java
@@ -1,15 +1,15 @@
 package cl.ucn.ei.pa.sistemaTransporteStarkon.logica;
 
 public interface SistemaTransporteStarkon {
-	public boolean ingresarLocalizacion(String nombre);
+	boolean ingresarLocalizacion(String nombre);
 
-	public boolean ingresarCliente(String rut, String nombre, String apellido, int saldo, String ciudadOrigen);
+	boolean ingresarCliente(String rut, String nombre, String apellido, int saldo, String ciudadOrigen);
 
-	public void ingresarDocumento(int codigo, String rutClienteRemitente, String rutClienteDestinatario, double peso, double grosor);
+	void ingresarDocumento(int codigo, String rutClienteRemitente, String rutClienteDestinatario, double peso, double grosor);
 
-	public void ingresarEncomienda(int codigo, String rutClienteRemitente, String rutClienteDestinatario, double peso, double largo, double ancho, double profundidad);
+	void ingresarEncomienda(int codigo, String rutClienteRemitente, String rutClienteDestinatario, double peso, double largo, double ancho, double profundidad);
 
-	public void ingresarValija(int codigo, String rutClienteRemitente, String rutClienteDestinatario, double peso, String material);
+	void ingresarValija(int codigo, String rutClienteRemitente, String rutClienteDestinatario, double peso, String material);
 
 	boolean isClienteRegistrado(String rutCliente);
 
